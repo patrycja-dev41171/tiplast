c2ac11b2adb7c4b42b65873da488cdd9006a5412
patrycja-dev41171 <patrycja.inska.contact@gmail.com>
2025-11-12 20:19:15 +0100

    feat: add misa terakota 25

--- database/products/products.ts
+++ database/products/products.ts
@@ -2,5 +2,12 @@ import fi9czarna from "./fi9czarna.json";
 import fi10czarna from "./fi10czarna.json";
 import fi12czarna from "./fi12czarna.json";
 import misa25biala from "./misa25biala.json";
+import misa25terakota from "./misa25terakota.json";
 
-export const products = [fi9czarna, fi10czarna, fi12czarna, misa25biala];
+export const products = [
+  fi9czarna,
+  fi10czarna,
+  fi12czarna,
+  misa25biala,
+  misa25terakota,
+];
