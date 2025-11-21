39bb1d996db5d49899c35a61e19d2c58b43d8218
patrycja-dev41171 <patrycja.inska.contact@gmail.com>
2025-11-21 22:00:47 +0100

    feat: connect to GTM

--- app.vue
+++ app.vue
@@ -2,5 +2,13 @@
   <NuxtLayout>
       <NuxtPage />
   </NuxtLayout>
+  <noscript>
+    <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-K8S3MXF5"
+      height="0"
+      width="0"
+      style="display:none;visibility:hidden">
+    </iframe>
+  </noscript>
 </template>
 
